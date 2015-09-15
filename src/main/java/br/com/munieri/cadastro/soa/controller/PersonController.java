1aa1b9f3413f7b352e18251ca6e4bd0ddd8e76ec
Murilo Niéri <murilo.nieri@hotmail.com>
2015-09-14 21:44:37 -0300

    criacao da controller

---
+++ src/main/java/br/com/munieri/cadastro/soa/controller/PersonController.java
@@ -0,0 +1,22 @@
+package br.com.munieri.cadastro.soa.controller;
+
+import br.com.munieri.cadastro.soa.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/pessoas")
+public class PersonController {
+
+    @Autowired
+    PersonDAO personDAO;
+
+    @RequestMapping(value = "/listar")
+    public ModelAndView listar() {
+        ModelAndView modelAndView = new ModelAndView("list");
+        modelAndView.addObject(personDAO.listar());
+        return modelAndView;
+    }
+}
