5a5f266abd497b9452550b0536b61116489a119d
Murilo Niéri <murilo.nieri@hotmail.com>
2015-09-14 21:43:40 -0300

    criacao do modelo

---
+++ src/main/java/br/com/munieri/cadastro/soa/model/Address.java
@@ -0,0 +1,68 @@
+package br.com.munieri.cadastro.soa.model;
+
+public class Address {
+
+    private Long id;
+    private String address;
+    private Integer number;
+    private String complement;
+    private String cep;
+    private String city;
+    private String state;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ src/main/java/br/com/munieri/cadastro/soa/model/Person.java
@@ -0,0 +1,61 @@
+package br.com.munieri.cadastro.soa.model;
+
+import java.util.Calendar;
+
+public class Person {
+
+    private Long id;
+    private String name;
+    private Calendar dateBirth;
+    private String phone;
+    private String mobile;
+    private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Calendar getDateBirth() {
+        return dateBirth;
+    }
+
+    public void setDateBirth(Calendar dateBirth) {
+        this.dateBirth = dateBirth;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
